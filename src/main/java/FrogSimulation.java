588c77353bfe3645dd34c7f346863def4ef809ff
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-04-25 21:40:30 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -15,13 +15,31 @@ public class FrogSimulation
 	
 	public boolean simulate()
 	{ 
-		/* to be implemented in part (a) */
+		int distance = 0;
+	  	int hops = 0;
+		while (hops < maxHops) {
+			distance += hopDistance();
+			hops++;
+			if (distance < 0) {
+				return false;
+			} else if (distance >= goalDistance) {
+				return true;
+			}
+		}
+		return false;
 
 	}
 	
 	public double runSimulations(int num)
 	{ 
-		/* to be implemented in part (b) */ 
+		double successes = 0.0;
+		int i = 0;
+		while (i < num) {
+			if (simulate()) {
+				successes++;
+			}
+		}
+		return successes/num;
 
 	}
 	
